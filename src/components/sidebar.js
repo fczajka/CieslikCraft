7cf0ab3536d52894c28073e7e955107cf07b3695
FranciszekCzajka <franekczajkaa@gmail.com>
2021-09-23 19:18:40 +0200

    Added 3 links to media

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -14,7 +14,7 @@ const Sidebar = () => {
     if(window.screen.width >= 2560) { 
 
       if(sidebarToResize.clientHeight < 194) {
-        sidebarToResize.style.height = "320px";
+        sidebarToResize.style.height = "438px";
       } else {
         sidebarToResize.style.height = "194px";
       }    
@@ -23,7 +23,7 @@ const Sidebar = () => {
     } else {
 
       if(sidebarToResize.clientHeight < 165) {
-        sidebarToResize.style.height = "272px";
+        sidebarToResize.style.height = "358px";
       } else {
         sidebarToResize.style.height = "165px";
       }    
@@ -102,12 +102,15 @@ const Sidebar = () => {
       <Dismiss/>
       <h1 className="menu">MENU</h1>
       <ul className="sidebar-list-links">
-        <li className="sidebar-links"><Link to="/">HOME</Link></li>
+        <li className="sidebar-links"><Link to="/"><p className="link">HOME</p></Link></li>
         <li className="sidebar-links"><Link to="/gallery">GALLERY</Link></li>
-        <li className="sidebar-links" onClick={toggleLinks}>MEDIA<i class="fas fa-angle-down"></i></li>        
+        <li className="sidebar-links" onClick={toggleLinks}>MEDIA<i className="fas fa-angle-down"></i></li>        
         <li className="media-links"><a href="https://btj.com.pl/2021/06/21/samodzielna-budowa-drewnianej-motorowki-czesc-i-poradnik-konstruowania-kadluba-na-przykladzie-nowej-lodzi-dolce-vita-z-domowej-stoczni-cieslik-craft/?fbclid=IwAR35I-nN-4V1Ffo2n0MtWgs-w5BSCWguvuOI-m_q2j17B0lYjQovBZPB8s4" target="_blank">Biuletyn Techniki Jachtowej</a></li>
         <li className="media-links"><a href="https://blog.dluta.pl/zbuduj-samodzielnie-motorowke-z-cieslik-craft/" target="_blank">Dłuta.pl</a></li>
         <li className="media-links"><a href="https://tomaszmiler.com/aktywny-weekend-marynarka-casualowa-krate-chino-lodce.html?fbclid=IwAR13s7_VgpfIpbIP5i9laZGy1X0dVGi_DLrDqJVDepWbvOuBZKSp-S_JRpg" target="_blank">Miler Menswear</a></li>
+        <li className="media-links"><a href="https://www.motorowy.com/artykul/21-12-2018/disco-volante-cieslik-craft?fbclid=IwAR1SLLIGAkAXK-2OV_o-qG8Z519A6UTPNdR9dg390Sr1H3PCVIPpCMFxT60" target="_blank">Jachting Motorowy</a></li>
+        <li className="media-links"><a href="http://www.sea-line.eu/pl/disco-volante/?fbclid=IwAR2weFH-8gXCd5Ik8kkD3RbxHqMujbeta-BI8s_BnieuAQiL8FaHil5s3wE" target="_blank">Sea-line</a></li>
+        <li className="media-links"><a href="http://www.jachtowe.com.pl/disco-volante.html?fbclid=IwAR1SLLIGAkAXK-2OV_o-qG8Z519A6UTPNdR9dg390Sr1H3PCVIPpCMFxT60" target="_blank">Jachtowe.com.pl</a></li>
       </ul>
       <ul className="sidebar-list-icons">
         <li className="sidebar-icon"><a href="https://www.instagram.com/cieslikcraft/" target="https://www.instagram.com/cieslikcraft/"><Img fluid={data.instagram.childImageSharp.fluid} alt="CieslikCraft - Instagram" /></a></li>
